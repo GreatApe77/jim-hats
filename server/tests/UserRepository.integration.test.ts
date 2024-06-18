d39ec51554ab1d6da1d6101ea8da8bc0b85cf469
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-18 14:19:14 -0300

    change commands

--- server/tests/UserRepository.integration.test.ts
+++ server/tests/UserRepository.integration.test.ts
@@ -5,9 +5,10 @@ import {
   SaveUserParams,
 } from "../src/users/repository/interfaces/IUserRepository";
 import { UserRepository } from "../src/users/repository/implementations/UserRepository";
-import prisma from "../src/db/__mocks__/prisma";
 import { IUser } from "../src/users/IUser";
-vi.mock("../src/db/prisma");
+import prisma from "../src/db/prisma";
+import { mockUsers } from "./constants/mockUsers";
+
 
 describe("UserRepository tests", () => {
   const mockedUser: IUser = {
@@ -21,18 +22,33 @@ describe("UserRepository tests", () => {
   };
 
   let usersRepo: IUserRepository;
-  beforeEach(() => {
+
+  beforeEach(async () => {
     usersRepo = new UserRepository();
+    await prisma.user.deleteMany()
+    await prisma.user.createMany({ data: mockUsers })
+
   });
 
-  it("Should create a user", async () => {});
-  it("should find by email", async () => {});
-  it("Should not find by email (does not exist)", async () => {});
-  it("Should find by id", async () => {});
-  it("Should not find by id (does not exist)", async () => {});
-  it("Should find by username", async () => {});
-  it("Should not find by username (does not exist)", async () => {});
-  it("Should find all users", async () => {});
-  it("Should update a user", async () => {});
-  it("Should delete a user", async () => {});
+  it("Should create a user", async () => {
+    const user: SaveUserParams = {
+      email: "test@test.com",
+      password: "abc123",
+      username: "usernametest",
+      profilePicture:"https://pic.com"
+    }
+    await usersRepo.save(user)
+    const createdUser = await usersRepo.findByEmail("test@test.com")
+    expect(createdUser?.email).to.be(user.email)
+
+  });
+  it("should find by email", async () => { });
+  it("Should not find by email (does not exist)", async () => { });
+  it("Should find by id", async () => { });
+  it("Should not find by id (does not exist)", async () => { });
+  it("Should find by username", async () => { });
+  it("Should not find by username (does not exist)", async () => { });
+  it("Should find all users", async () => { });
+  it("Should update a user", async () => { });
+  it("Should delete a user", async () => { });
 });
