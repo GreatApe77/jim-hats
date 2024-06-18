00fc49edfee808c72121e1395b32a5702d20721a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-18 09:29:19 -0300

    interface for user repository

---
+++ server/src/users/repository/interfaces/IUserRepository.ts
@@ -0,0 +1,17 @@
+import { IUser } from "../../IUser";
+
+export type SaveUserParams = Pick<IUser,"email"|"password"|"createdAt"|"profilePicture">;
+export type UpdateUserParams = Pick<IUser,"email"|"password"|"profilePicture">;
+export type PaginationParams = {
+    offset?:number;
+    limit?:number;
+}
+export interface IUserRepository {
+    save(user:SaveUserParams): Promise<void>;
+    findByEmail(email:string): Promise<IUser | null>;
+    findById(id:number): Promise<IUser | null>;
+    findByUsername(username:string): Promise<IUser | null>;
+    findAll(paginationParams:PaginationParams): Promise<IUser[]>;
+    update(id:number, user:UpdateUserParams): Promise<void>;
+    delete(id:number): Promise<void>;
+}
