a10ba2142fe0eb44e56eb9283fb94aed272862b1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-18 21:37:58 -0300

    remove files

--- server/tests/constants/mockUsers.ts
+++ server/tests/constants/mockUsers.ts
@@ -1,31 +1,28 @@
-import { IUser } from "../../src/users/IUser";
+import { SaveUserParams } from "../../src/users/repository/interfaces/IUserRepository"
 
-export const mockUsers: IUser[] = [
+export const mockUsers: SaveUserParams[] = [
     {
-        id: 1,
+
         username: 'john_doe',
         email: 'john@example.com',
         password: 'securepassword1',
         profilePicture: 'john_profile.jpg',
-        createdAt: new Date('2023-01-01T10:00:00Z'),
-        updatedAt: new Date('2023-06-01T10:00:00Z')
+
     },
     {
-        id: 2,
+
         username: 'jane_smith',
         email: 'jane@example.com',
         password: 'securepassword2',
         profilePicture: 'jane_profile.jpg',
-        createdAt: new Date('2023-02-01T11:00:00Z'),
-        updatedAt: new Date('2023-06-02T11:00:00Z')
+
     },
     {
-        id: 3,
+
         username: 'bob_jones',
         email: 'bob@example.com',
         password: 'securepassword3',
         profilePicture: null,
-        createdAt: new Date('2023-03-01T12:00:00Z'),
-        updatedAt: new Date('2023-06-03T12:00:00Z')
+
     }
 ];
