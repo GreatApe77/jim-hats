918883000451f299c1b7757defc7dd0c86217bb7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-06-20 20:06:15 -0300

    setup script for e2e tests

--- server/tests/integration/UserRepository.e2e.test.ts
+++ server/tests/integration/UserRepository.e2e.test.ts
@@ -9,8 +9,19 @@ import { UserRepository } from "../../src/users/repository/implementations/UserR
 
 
 describe("UserRepository tests", () => {
-
-  it("Should create a bruh", async () => {
-    expect(true).toBe(true)
+  let usersRepo: IUserRepository = new UserRepository();
+  it("Should create a a user", async () => {
+    const user = {
+      email: "email",
+      username: "username",
+      password: "password",
+      profilePicture: "profilePicture"
+    }
+    expect(await usersRepo.save(user)).to.not.throw
+    const createdUser = await usersRepo.findByEmail(user.email)
+    expect(createdUser?.email).to.equal(user.email)
+  })
+  it("Should find a user by email", async () => {
+    expect(true).to.equal(true)
   })
 })
