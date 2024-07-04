4f38ee9741e9fc7fa313ea64092f992af3534ebb
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-07-04 08:08:57 -0300

    fix position

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,50 +1,36 @@
-import { Box, Button, Container, Grid, Link, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Link, Stack, Typography } from "@mui/material";
 import Image from "next/image";
+
 export default function Home() {
   return (
-    <main >
+    <main>
       <Container maxWidth="md" sx={{
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center"
-
-      }}
-      >
-
-
-
-        <Stack direction={"column"} spacing={2} flexGrow={1} alignSelf={"center"} textAlign={"center"} >
-          <Box display={"flex"} flexDirection={"column"} height={"100%"} margin={"auto"}>
-
-            <Box textAlign={"center"} >
-              <Image style={{
-                width: "100%",
-                maxWidth: "200px",
-              }} src={"/rat.jpg"} alt="rat" width={"200"} height={"200"} />
-
-            </Box>
-
-            <Typography variant="h5">Welcome to
-              <Box fontWeight={"bold"} display={"inline"}>
-                {" "}Jim Hats
-
-              </Box>
-            </Typography>
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}>
+        <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+          <Box>
+            <Image style={{
+              width: "100%",
+              maxWidth: "200px",
+            }} src={"/rat.jpg"} alt="rat" width={"200"} height={"200"} />
           </Box>
-        </Stack>
-        <Box  >
-          <Stack direction={"column"} spacing={1} >
-            <Button variant="contained" color="primary" size="large" fullWidth>Create Account</Button>
-            <Typography gutterBottom variant="body1">Already have an account? <Link underline="hover" color="primary">Login</Link></Typography>
-
-          </Stack>
+          <Typography variant="h5">
+            Welcome to
+            <Box fontWeight={"bold"} display={"inline"}>{" "}Jim Hats</Box>
+          </Typography>
         </Box>
 
-
-
-
+        <Box sx={{ width: '100%', mt: 'auto' }}>
+          <Stack direction={"column"} spacing={1}>
+            <Button variant="contained" color="primary" size="large" fullWidth>Create Account</Button>
+            <Typography gutterBottom variant="body1">Already have an account? <Link underline="hover" color="primary">Login</Link></Typography>
+          </Stack>
+        </Box>
       </Container>
-    </main >
+    </main>
   );
 }
