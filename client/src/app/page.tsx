ef7dea6f3fe68bd6728c6603e3faa6dada92a747
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-07-04 08:05:08 -0300

    trying to center div

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Link, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Link, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 export default function Home() {
   return (
@@ -7,36 +7,40 @@ export default function Home() {
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        justifyContent: "center",
+        justifyContent: "center"
 
       }}
       >
 
 
-        <Stack direction={"column"} spacing={2} mt={24} textAlign={"center"} flexGrow={1}>
 
-          <Box textAlign={"center"} >
-            <Image style={{
-              width: "100%",
-              maxWidth: "200px",
-            }} src={"/rat.jpg"} alt="rat" width={"200"} height={"200"} />
+        <Stack direction={"column"} spacing={2} flexGrow={1} alignSelf={"center"} textAlign={"center"} >
+          <Box display={"flex"} flexDirection={"column"} height={"100%"} margin={"auto"}>
 
-          </Box>
-
-          <Typography variant="h5">Welcome to
-            <Box fontWeight={"bold"} display={"inline"}>
-              {" "}Jim Hats
+            <Box textAlign={"center"} >
+              <Image style={{
+                width: "100%",
+                maxWidth: "200px",
+              }} src={"/rat.jpg"} alt="rat" width={"200"} height={"200"} />
 
             </Box>
-          </Typography>
+
+            <Typography variant="h5">Welcome to
+              <Box fontWeight={"bold"} display={"inline"}>
+                {" "}Jim Hats
+
+              </Box>
+            </Typography>
+          </Box>
         </Stack>
-        <Box display={"flex"} flexDirection={"column"}  justifyContent={"end"}>
+        <Box  >
           <Stack direction={"column"} spacing={1} >
-            <Button variant="contained" color="primary" fullWidth>Create Account</Button>
+            <Button variant="contained" color="primary" size="large" fullWidth>Create Account</Button>
             <Typography gutterBottom variant="body1">Already have an account? <Link underline="hover" color="primary">Login</Link></Typography>
 
           </Stack>
         </Box>
+
 
 
 
