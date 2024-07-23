abafb23917783771aa31a2cc9e2bc790444cb1e1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-07-23 09:06:39 -0300

    finish banner design

--- client/src/app/application/challenges/[id]/page.tsx
+++ client/src/app/application/challenges/[id]/page.tsx
@@ -1,18 +1,36 @@
 "use client";
+import ChallengeBanner from "@/components/ChallengeBanner";
 import { useChallenge } from "@/hooks/useChallenge";
+import { useLoggedUser } from "@/hooks/useLoggedUser";
 import { Container, Typography } from "@mui/material";
 import { useParams } from "next/navigation";
 
 export default function ChallengePage() {
   const params = useParams();
   const challengeId = params.id as string;
+  const {data:loggedUserResponse} = useLoggedUser();
+  
   const { data: challengeResponse } = useChallenge(challengeId);
   const challenge = challengeResponse?.response.data;
+  const user = loggedUserResponse?.response.data;
   return (
     <>
     <Container maxWidth="sm">
 
       <Typography variant="h6">{challenge?.name}</Typography>
+      <ChallengeBanner 
+        challengeImage="https://images.pexels.com/photos/3077882/pexels-photo-3077882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        endDate={challenge?.endAt!}
+        you={{
+          count: 5,
+          profilePicture: user?.profilePicture || "",
+        }}
+        leader={{
+          count: 20,
+          profilePicture: "https://images.pexels.com/photos/3077882/pexels-photo-3077882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }}
+
+      />
     </Container>
     </>
   );
---
+++ client/src/components/ChallengeBanner.tsx
@@ -0,0 +1,107 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
+import dayjs from "dayjs";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+export default function ChallengeBanner({
+  challengeImage,
+  endDate,
+  you,
+  leader,
+}: {
+  challengeImage: string;
+  endDate: string;
+  you: {
+    count: number;
+    profilePicture: string;
+  };
+  leader: {
+    count: number;
+    profilePicture: string;
+  };
+}) {
+  return (
+    <>
+      <Card>
+        <CardMedia
+          component="img"
+          height="194"
+          image={challengeImage??"https://images.pexels.com/photos/3077882/pexels-photo-3077882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
+          alt="Challenge Image"
+        />
+        <CardContent
+          sx={{
+            padding: 0,
+          }}
+        >
+          <Stack direction="row" justifyContent={"center"} spacing={0}>
+            <CardHeader
+              sx={{
+                paddingX: 1,
+              }}
+              avatar={
+                <Avatar
+                  src={leader.profilePicture}
+                  alt="Leader"
+                
+                    sx={{
+                        width: 36,
+                        height: 36,
+                    }}
+                />
+              }
+              title={you.count}
+              subheader={<Typography variant="caption">
+               Leader
+              </Typography>}
+            />
+
+            <CardHeader
+              sx={{
+                paddingX: 1,
+              }}
+              avatar={
+                <Avatar
+                sx={{
+                    width: 36,
+                    height: 36,
+                }}
+                  src={you.profilePicture}
+                  alt="You"
+                />
+              }
+              title={you.count}
+              subheader={<Typography variant="caption">
+                You
+              </Typography>}
+            />
+
+            <CardHeader
+              sx={{
+                paddingX: 1,
+              }}
+              avatar={<Avatar alt="Remy Sharp" sx={{
+                bgcolor:"inherit",
+                color:"white",
+                width: 36,
+                height: 36,
+              }}>
+                <CalendarMonthIcon/>
+              </Avatar>}
+              title={dayjs(endDate).diff(dayjs(), "day")}
+              subheader={<Typography variant="caption">
+                days left
+              </Typography>}
+            />
+          </Stack>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
