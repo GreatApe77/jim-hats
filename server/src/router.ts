b4a8cf0e8d25edd1cd7a51d3bafd88ef815d706c
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-07-31 11:45:01 -0300

    add standard middleware for not found

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,6 +3,7 @@ import { authRouter } from "./domain/auth/router.js";
 import { fileUploadRouter } from "./domain/file-upload/router.js";
 import { gymChallengesRouter } from "./domain/gym-challenges/router.js";
 import { usersRouter } from "./domain/users/router.js";
+import { errorResponse } from "./utils/responses.js";
 
 const router = Router();
 
@@ -10,4 +11,10 @@ router.use(authRouter);
 router.use("/users", usersRouter);
 router.use("/uploads", fileUploadRouter);
 router.use("/gym-challenges", gymChallengesRouter);
+router.get("/health", (req, res) => {
+    return res.status(200).json({ message: "OK" });
+})
+router.use("*",(req,res)=>{
+    return res.status(404).send(errorResponse("Route not found"))
+})
 export { router };
