31696db3361d18bcdd56e582682fdfbb54ce078e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-09-13 12:22:22 -0300

    remove js config file

--- server/vitest.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-export default defineConfig({
-    test: {
-        //exclude: ["**/__mocks__/**"],
-        coverage: {
-            exclude: ["**/__mocks__/**"]
-        }
-    },
-});
