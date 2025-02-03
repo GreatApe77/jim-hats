dfb45031ceac9ca3ab64725723a252461baefe89
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-03 16:41:56 -0300

    fix color

--- jim_hats_mobile/lib/ui/views/create_account/create_account_page.dart
+++ jim_hats_mobile/lib/ui/views/create_account/create_account_page.dart
@@ -39,7 +39,7 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                 height: 100,
                 width: 100,
                 child: Material(
-                  color: Theme.of(context).colorScheme.surfaceDim,
+                  color: Theme.of(context).colorScheme.surfaceContainer,
                   shape: CircleBorder(),
                   child: InkWell(
                     customBorder: CircleBorder(),
