2afc8f1ec2e8721ad0f70e743d52e4b86bd46cea
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-12 16:19:59 -0300

    add material theme

--- jim_hats_mobile/lib/app.dart
+++ jim_hats_mobile/lib/app.dart
@@ -3,29 +3,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jim_hats_mobile/locator.dart';
 import 'package:jim_hats_mobile/routing/app_router.dart';
 import 'package:jim_hats_mobile/ui/theme/bloc/theme_bloc.dart';
+import 'package:jim_hats_mobile/ui/theme/fonts.dart';
+import 'package:jim_hats_mobile/ui/theme/materia_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = createTextTheme(context, "Inter", "Inter");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
     return BlocBuilder<ThemeBloc, ThemeState>(
       bloc: locator.get<ThemeBloc>(),
       builder: (context, state) {
         return MaterialApp(
-          darkTheme: ThemeData(
-            colorScheme:ColorScheme.fromSeed(seedColor: Colors.red,brightness: Brightness.dark)
-          ),
-          theme: ThemeData(
-            
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)
-          ),
-          onGenerateRoute: AppRouter.ongenerateRoute,
-          initialRoute: AppRouter.initialRoute,
-        
-          //themeMode: ThemeMode.dark,
-          themeMode: state is ThemeDark?ThemeMode.dark:ThemeMode.light
-        );
+            darkTheme: theme.dark(),
+            theme: theme.light(),
+            onGenerateRoute: AppRouter.ongenerateRoute,
+            initialRoute: AppRouter.initialRoute,
+
+            //themeMode: ThemeMode.dark,
+            themeMode: state is ThemeDark ? ThemeMode.dark : ThemeMode.light);
       },
     );
   }
