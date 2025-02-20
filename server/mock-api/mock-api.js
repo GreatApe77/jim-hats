185faab14ba32f6602057849aae6f7b2ca906219
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-20 16:22:19 -0300

    create mock of ranking endpoint

--- server/mock-api/mock-api.js
+++ server/mock-api/mock-api.js
@@ -132,6 +132,59 @@ app.get(
     return res.status(200).json(gymChallenges)
   }
 )
+app.get("/gym-challenges/:challengeId/ranking",(req,res)=>{
+  const rankings = [
+    {
+      id: 1,
+      username: 'User1',
+      profilePicture: 'url1',
+      logCount: 10
+    },
+    {
+      id: 2,
+      username: 'User2',
+      profilePicture: 'url2',
+      logCount: 9
+    },
+    {
+      id: 3,
+      username: 'User3',
+      profilePicture: 'url3',
+      logCount: 8
+    },
+    {
+      id: 4,
+      username: 'User4',
+      profilePicture: 'url4',
+      logCount: 7
+    },
+    {
+      id: 5,
+      username: 'User5',
+      profilePicture: 'url5',
+      logCount: 6
+    },
+    {
+      id: 6,
+      username: 'User6',
+      profilePicture: 'url6',
+      logCount: 5
+    },
+    {
+      id: 7,
+      username: 'User7',
+      profilePicture: 'url7',
+      logCount: 4
+    },
+    {
+      id: 8,
+      username: 'User8',
+      profilePicture: 'url8',
+      logCount: 3
+    }
+  ];
+  return res.status(200).json(rankings)
+})
 app.post("/uploads",uploadPhoto,(req,res)=>{
   const fullPath= req.body.fullPath
   return res.status(200).json(successResponse(MESSAGES.CREATED,{fullPath}))
