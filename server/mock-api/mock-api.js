4db94d135bc60b34d837b289370b1759bae91124
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-20 16:15:11 -0300

    get challenges of user endpoint

--- server/mock-api/mock-api.js
+++ server/mock-api/mock-api.js
@@ -92,6 +92,46 @@ app.get("/users/me",(req,res)=>{
   }
   return res.status(200).json(user)
 })
+app.get(
+  "/users/:userId/gym-challenges",(req,res)=>{
+    const gymChallenges = [
+      {
+        id: 5,
+        name: 'Challenge 5',
+        description: 'Challenge Description 5',
+        image: 'https://avatars.githubusercontent.com/u/97452495?s=200&v=4',
+        joinId: null,
+        createdAt: new Date(),
+        startAt: new Date('2024-01-01T00:00:00Z'),
+        endAt: new Date('2027-01-01T00:00:00Z'),
+        creatorId: 1
+      },
+      {
+        id: 6,
+        name: 'Challenge 6',
+        description: 'Challenge Description 6',
+        image: 'https://avatars.githubusercontent.com/u/98452395?s=200&v=4',
+        joinId: null,
+        createdAt: new Date(),
+        startAt: new Date(Date.now() + 20000000),
+        endAt: new Date(Date.now() + 80000000),
+        creatorId: 1
+      },
+      {
+        id: 7,
+        name: 'Challenge 7',
+        description: 'Challenge Description 7',
+        image: null,
+        joinId: null,
+        createdAt: new Date(),
+        startAt: new Date(Date.now() + 20000000),
+        endAt: new Date(Date.now() + 80000000),
+        creatorId: 1
+      }
+    ];
+    return res.status(200).json(gymChallenges)
+  }
+)
 app.post("/uploads",uploadPhoto,(req,res)=>{
   const fullPath= req.body.fullPath
   return res.status(200).json(successResponse(MESSAGES.CREATED,{fullPath}))
