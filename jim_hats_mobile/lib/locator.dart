ffa31b7fb2923faa931682e8841082776461d37b
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-27 14:13:02 -0300

    create factories instead of singletons in Blocs

--- jim_hats_mobile/lib/locator.dart
+++ jim_hats_mobile/lib/locator.dart
@@ -82,69 +82,60 @@ Future<void> setupDependencies() async {
 
   //Cubits
 
-  locator
-    ..registerFactory<InternetConnectivityCubit>(
-      () => InternetConnectivityCubit(),
-    )
-    ..registerFactory<JoinGroupPageCubit>(
-      () => JoinGroupPageCubit(
-          gymChallengesRepository: locator.get<GymChallengesRepository>()),
-    )
-    ..registerSingleton<CreateChallengePageCubit>(CreateChallengePageCubit(
+locator
+  ..registerFactory<InternetConnectivityCubit>(
+    () => InternetConnectivityCubit(),
+  )
+  ..registerFactory<JoinGroupPageCubit>(
+    () => JoinGroupPageCubit(
+        gymChallengesRepository: locator.get<GymChallengesRepository>()),
+  )
+  ..registerFactory<CreateChallengePageCubit>(() => CreateChallengePageCubit(
+    gymChallengesRepository: locator.get<GymChallengesRepository>(),
+    uploadRepository: locator.get<UploadRepository>(),
+  ))
+  ..registerFactory<HomePageCubit>(() => HomePageCubit(
+      loggedUserRepository: locator.get<LoggedUserRepository>()))
+  ..registerFactoryParam<NewCheckInPageCubit, XFile, dynamic>(
+    (param1, param2) => NewCheckInPageCubit(
+        checkInPhoto: param1,
+        uploadRepository: locator.get<UploadRepository>(),
+        exerciseLogsRepositoy: locator.get<ExerciseLogsRepository>()),
+  )
+  ..registerFactory<GymChallengeDetailsPageCubit>(() => GymChallengeDetailsPageCubit(
       gymChallengesRepository: locator.get<GymChallengesRepository>(),
-      uploadRepository: locator.get<UploadRepository>(),
-    ))
-    ..registerSingleton<HomePageCubit>(HomePageCubit(
-        loggedUserRepository: locator.get<LoggedUserRepository>()))
-    ..registerFactoryParam<NewCheckInPageCubit, XFile, dynamic>(
-      (param1, param2) => NewCheckInPageCubit(
-          checkInPhoto: param1,
-          uploadRepository: locator.get<UploadRepository>(),
-          exerciseLogsRepositoy: locator.get<ExerciseLogsRepository>()),
-    )
-    ..registerSingleton<GymChallengeDetailsPageCubit>(
-        GymChallengeDetailsPageCubit(
-            gymChallengesRepository: locator.get<GymChallengesRepository>(),
-            loggedUserRepository: locator.get<LoggedUserRepository>()))
-    ..registerSingleton<AuthCubit>(AuthCubit(
-        authRepository: locator.get<AuthRepository>(),
-        loggedUserRepository: locator.get<LoggedUserRepository>()))
-    ..registerSingleton<SignInPageBloc>(
-        SignInPageBloc(authRepository: locator.get<AuthRepository>()))
-    ..registerSingleton<CreateAccountPageCubit>(CreateAccountPageCubit(
-        authRepository: locator.get<AuthRepository>(),
-        uploadRepository: locator.get<UploadRepository>()))
-    ..registerSingleton<GymChallengePageCubit>(
-      GymChallengePageCubit(
-          gymChallengesRepository: locator.get<GymChallengesRepository>(),
-          loggedUserRepository: locator.get<LoggedUserRepository>(),
-          exerciseLogsRepository: locator.get<ExerciseLogsRepository>()),
-    )
-    ..registerSingleton<AppDrawerCubit>(
-      AppDrawerCubit(
-          gymChallengesRepository: locator.get<GymChallengesRepository>(),
-          loggedUserRepository: locator.get<LoggedUserRepository>()),
-    )
-    ..registerSingleton(
-      SettingsCubit(
-          loggedUserRepository: locator.get<LoggedUserRepository>(),
-          uploadRepository: locator.get<UploadRepository>()),
-    )
-    ..registerSingleton<ThemeBloc>(ThemeBloc(
-        settingsRepository: locator.get<SettingsRepository>(),
-        themeState: locator.get<SettingsRepository>().settings.isDarkTheme
-            ? ThemeDark()
-            : ThemeLight()))
-    ..registerFactory<UserStatsCubit>(
-      () => UserStatsCubit(
-        exerciseLogRepository: locator.get<ExerciseLogsRepository>(),
-        loggedUserRepository: locator.get<LoggedUserRepository>(),
-      ),
-    )
-    ..registerSingleton<RankingPageCubit>(RankingPageCubit(
-        //gymChallengesRepository: null,
-        //loggedUserRepository: null
-        ));
+      loggedUserRepository: locator.get<LoggedUserRepository>()))
+  ..registerFactory<AuthCubit>(() => AuthCubit(
+      authRepository: locator.get<AuthRepository>(),
+      loggedUserRepository: locator.get<LoggedUserRepository>()))
+  ..registerFactory<SignInPageBloc>(() => SignInPageBloc(
+      authRepository: locator.get<AuthRepository>()))
+  ..registerFactory<CreateAccountPageCubit>(() => CreateAccountPageCubit(
+      authRepository: locator.get<AuthRepository>(),
+      uploadRepository: locator.get<UploadRepository>()))
+  ..registerFactory<GymChallengePageCubit>(() => GymChallengePageCubit(
+      gymChallengesRepository: locator.get<GymChallengesRepository>(),
+      loggedUserRepository: locator.get<LoggedUserRepository>(),
+      exerciseLogsRepository: locator.get<ExerciseLogsRepository>()))
+  ..registerFactory<AppDrawerCubit>(() => AppDrawerCubit(
+      gymChallengesRepository: locator.get<GymChallengesRepository>(),
+      loggedUserRepository: locator.get<LoggedUserRepository>()))
+  ..registerFactory<SettingsCubit>(() => SettingsCubit(
+      loggedUserRepository: locator.get<LoggedUserRepository>(),
+      uploadRepository: locator.get<UploadRepository>()))
+  ..registerFactory<ThemeBloc>(() => ThemeBloc(
+      settingsRepository: locator.get<SettingsRepository>(),
+      themeState: locator.get<SettingsRepository>().settings.isDarkTheme
+          ? ThemeDark()
+          : ThemeLight()))
+  ..registerFactory<UserStatsCubit>(() => UserStatsCubit(
+    exerciseLogRepository: locator.get<ExerciseLogsRepository>(),
+    loggedUserRepository: locator.get<LoggedUserRepository>(),
+  ))
+  ..registerFactory<RankingPageCubit>(() => RankingPageCubit(
+      //gymChallengesRepository: null,
+      //loggedUserRepository: null
+      ));
 }
 
 Future<void> loadSettings() async {
