afd8e1ee50d2465bfa50f1d5468d489a84f154fd
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-02 12:38:11 -0300

    http abstraction wip

---
+++ jim_hats_mobile/lib/core/network/http_service.dart
@@ -0,0 +1,7 @@
+abstract class HttpService {
+  Future<Map<String, dynamic>> get(String path, {Map<String, dynamic>? queryParameters});
+  Future<Map<String, dynamic>> post(String path, {dynamic data});
+  Future<Map<String, dynamic>> put(String path, {dynamic data});
+  Future<Map<String, dynamic>> patch(String path, {dynamic data});
+  Future<void> delete(String path);
+}
