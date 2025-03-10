6be059804a24dbfa34733ac037315af27b1f4ebb
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-10 15:57:42 -0300

    create edit check in page arguments

---
+++ jim_hats_mobile/lib/presentation/views/edit_check_in/edit_check_in_page_arguments.dart
@@ -0,0 +1,8 @@
+import 'package:jim_hats_mobile/data/exercise_logs/models/exercise_log.dart';
+
+class EditCheckInPageArguments {
+  ExerciseLog exerciseLog;
+  EditCheckInPageArguments({
+    required this.exerciseLog,
+  });
+}
