45b1edbacc2de02af271c96b054ad8bb455c67a7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-10 15:53:03 -0300

    edit check in page cubit wip

---
+++ jim_hats_mobile/lib/presentation/cubits/edit_check_in_page/edit_check_in_page_cubit.dart
@@ -0,0 +1,70 @@
+import 'package:bloc/bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:jim_hats_mobile/core/utils/application_exception.dart';
+import 'package:jim_hats_mobile/core/utils/nullable.dart';
+import 'package:jim_hats_mobile/data/exercise_logs/dtos/update_exercise_log_dto.dart';
+import 'package:jim_hats_mobile/data/exercise_logs/models/exercise_log.dart';
+import 'package:jim_hats_mobile/data/exercise_logs/repositories/exercise_logs_repository.dart';
+
+part 'edit_check_in_page_state.dart';
+
+class EditCheckInPageCubit extends Cubit<EditCheckInPageState> {
+  
+  final ExerciseLogsRepository _exerciseLogsRepository;
+  EditCheckInPageCubit({
+    required ExerciseLogsRepository exerciseLogsRepository,
+  })  : _exerciseLogsRepository = exerciseLogsRepository,
+        super(
+          EditCheckInPageState(
+            errorMessage: '',
+            title: '',
+            description: '',
+            image: null,
+            status: EditCheckInPageStatus.idle,
+          ),
+        );
+
+  void updateTitle(String title) {
+    emit(
+      state.copyWith(
+        title: title,
+      ),
+    );
+  }
+
+  void updateDescription(String description) {
+    emit(
+      state.copyWith(description: Nullable(description)),
+    );
+  }
+
+  void updateImage(XFile? image) {
+    emit(
+      state.copyWith(
+        image: Nullable(image),
+      ),
+    );
+  }
+
+  void submitForm() async {
+    try {
+      emit(
+        state.copyWith(status: EditCheckInPageStatus.loading),
+      );
+    } on ApplicationException catch (e) {
+      emit(
+        state.copyWith(
+          errorMessage: e.getMessage(),
+          status: EditCheckInPageStatus.error,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          errorMessage: 'Unknown error while updating check in',
+          status: EditCheckInPageStatus.error,
+        ),
+      );
+    }
+  }
+}
