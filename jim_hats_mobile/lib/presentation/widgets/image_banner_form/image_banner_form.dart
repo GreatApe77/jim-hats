f2c7bd7845d8400758ba31f971c17f5042c7ab45
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-12 16:07:28 -0300

    remove comments

--- jim_hats_mobile/lib/presentation/widgets/image_banner_form/image_banner_form.dart
+++ jim_hats_mobile/lib/presentation/widgets/image_banner_form/image_banner_form.dart
@@ -92,76 +92,8 @@ class ImageBannerForm extends StatelessWidget {
         ],
       ),
     );
-    // return SizedBox(
-    //   height: 255,
-    //   child: Stack(fit: StackFit.expand, children: [
-    //     BlocBuilder<CreateChallengePageCubit, CreateChallengePageState>(
-    //       bloc: context.read<CreateChallengePageCubit>(),
-    //       buildWhen: (previous, current) => previous.image != current.image,
-    //       builder: (context, state) {
-    //         if (state.image == null) {
-    //           return Align(
-    //             alignment: Alignment.center,
-    //             child: InkWell(
-    //               borderRadius: BorderRadius.circular(10),
-    //               onTap: () {
-    //                 _takePicture(context.read<CreateChallengePageCubit>());
-    //               },
-    //               child: Ink(
-    //                 decoration: BoxDecoration(
-    //                   borderRadius: BorderRadius.circular(10),
-    //                   color: Theme.of(context).colorScheme.onSurface,
-    //                 ),
-    //                 height: 200,
-    //               ),
-    //             ),
-    //           );
-    //         }
-    //         return Align(
-    //           alignment: Alignment.center,
-    //           child: InkWell(
-    //             borderRadius: BorderRadius.circular(10),
-    //             onTapDown: (details) {
-    //               _showMenu(
-    //                 context,
-    //                 details.globalPosition,
-    //                 context.read<CreateChallengePageCubit>(),
-    //               );
-    //             },
-    //             child: Ink(
-    //               decoration: BoxDecoration(
-    //                 borderRadius: BorderRadius.circular(10),
-    //                 image: DecorationImage(
-    //                   fit: BoxFit.cover,
-    //                   image: FileImage(File(state.image!.path)),
-    //                 ),
-    //               ),
-    //               height: 200,
-    //             ),
-    //           ),
-    //         );
-    //       },
-    //     ),
-    //     Align(
-    //       alignment: Alignment.bottomCenter,
-    //       child: Padding(
-    //         padding: const EdgeInsets.all(8.0),
-    //         child: CircleAvatar(
-    //           backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
-    //           child: Icon(Icons.camera_alt),
-    //         ),
-    //       ),
-    //     )
-    //   ]),
-    // );
+
   }
 
-  // void _takePicture(CreateChallengePageCubit cubit) {
-  //   // Implement the picture taking logic here
-  // }
 
-  // void _showMenu(
-  //     BuildContext context, Offset position, CreateChallengePageCubit cubit) {
-  //   // Implement the menu showing logic here
-  // }
 }
