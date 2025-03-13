b1860fc4560e3ebf5f5eb9fb1bc4a4072b05b23e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-13 16:33:08 -0300

    code formating

--- jim_hats_mobile/lib/presentation/views/gym_challenge_details/gym_challenge_details_page.dart
+++ jim_hats_mobile/lib/presentation/views/gym_challenge_details/gym_challenge_details_page.dart
@@ -7,7 +7,10 @@ import 'package:jim_hats_mobile/presentation/cubits/gym_challenge_details_page/g
 import 'package:jim_hats_mobile/presentation/widgets/user_circle_avatar/user_circle_avatar.dart';
 
 class GymChallengeDetailsPage extends StatelessWidget {
-  const GymChallengeDetailsPage({super.key, required this.pageArguments});
+  const GymChallengeDetailsPage({
+    super.key,
+    required this.pageArguments,
+  });
   final GymChallengePageArguments pageArguments;
   @override
   Widget build(BuildContext context) {
@@ -27,95 +30,94 @@ class GymChallengeDetailsView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-          child: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: AppSpacings.horizontalPadding.toDouble()),
-        child: BlocBuilder<GymChallengeDetailsPageCubit,
-            GymChallengeDetailsPageState>(
-          bloc: context.read<GymChallengeDetailsPageCubit>(),
-          builder: (context, state) {
-            if (state is GymChallengeDetailsPageLoadDataInProgress) {
-              return Center(
-                child: const CircularProgressIndicator(),
-              );
-            }
-            if (state is GymChallengeDetailsPageLoadError) {
-              return Center(
-                child: const Text('Error'),
-              );
-            }
-            if (state is GymChallengeDetailsPageLoadSuccess) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    child: Text(
-                      state.challenge.name,
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      textAlign: TextAlign.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppSpacings.horizontalPadding.toDouble(),
+          ),
+          child: BlocBuilder<GymChallengeDetailsPageCubit,
+              GymChallengeDetailsPageState>(
+            bloc: context.read<GymChallengeDetailsPageCubit>(),
+            builder: (context, state) {
+              if (state is GymChallengeDetailsPageLoadDataInProgress) {
+                return Center(
+                  child: const CircularProgressIndicator(),
+                );
+              }
+              if (state is GymChallengeDetailsPageLoadError) {
+                return Center(
+                  child: const Text('Error'),
+                );
+              }
+              if (state is GymChallengeDetailsPageLoadSuccess) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        state.challenge.name,
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                  ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    title: Text(state.admin.username),
-                    subtitle: Text('Group Admin'),
-                    leading: UserCircleAvatar(
-                      avatarUrl: state.admin.profilePicture,
-                      username: state.admin.username,
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text(state.admin.username),
+                      subtitle: Text('Group Admin'),
+                      leading: UserCircleAvatar(
+                        avatarUrl: state.admin.profilePicture,
+                        username: state.admin.username,
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('${state.members.length} Members'),
-                      InkWell(
-                        child: Text(
-                          'All',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 60,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (context, index) {
-                        return SizedBox(
-                          width: 10,
-                        );
-                      },
-                      
-                      itemCount: state.members.length,
-                      itemBuilder: (context, index) {
-                        
-                        return UserCircleAvatar(
-                          avatarUrl: state.members[index].profilePicture,
-                          username: state.members[index].username,
-                        );
-                      },
-                      
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('${state.members.length} Members'),
+                        InkWell(
+                          child: Text(
+                            'All',
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary),
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    onTap: () {},
-                    textColor: Theme.of(context).colorScheme.error,
-                    leading: Icon(
-                      Icons.logout,
-                      color: Theme.of(context).colorScheme.error,
+                    SizedBox(
+                      height: 60,
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        separatorBuilder: (context, index) {
+                          return SizedBox(
+                            width: 10,
+                          );
+                        },
+                        itemCount: state.members.length,
+                        itemBuilder: (context, index) {
+                          return UserCircleAvatar(
+                            avatarUrl: state.members[index].profilePicture,
+                            username: state.members[index].username,
+                          );
+                        },
+                      ),
                     ),
-                    title: Text('Leave'),
-                  )
-                ],
-              );
-            }
-            return SizedBox.shrink();
-          },
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      onTap: () {},
+                      textColor: Theme.of(context).colorScheme.error,
+                      leading: Icon(
+                        Icons.logout,
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                      title: Text('Leave'),
+                    )
+                  ],
+                );
+              }
+              return SizedBox.shrink();
+            },
+          ),
         ),
-      )),
+      ),
     );
   }
 }
--- jim_hats_mobile/lib/presentation/views/join_group/join_group_page.dart
+++ jim_hats_mobile/lib/presentation/views/join_group/join_group_page.dart
@@ -31,100 +31,99 @@ class _JoinGroupViewState extends State<JoinGroupView> {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-          child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: AppSpacings.horizontalPadding.toDouble()),
-              child: Form(
-                key: _formKey,
-                child: ListView(
-                  children: [
-                    Text(
-                      'Join Group',
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      'Enter a group code to join',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    TextFormField(
-                      
-                      validator: (value) =>
-                          FormValidators.validateGroupCode(value),
-                      onChanged: (value) {
-                        //widget.joinGroupPageCubit.updateGroupCode(value);
-                        context.read<JoinGroupPageCubit>().updateGroupCode(value);
-                      
-                      },
-                      decoration: InputDecoration(
-                          label: Text('Group code'),
-                          border: OutlineInputBorder()),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      'Looks like',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      '• afb78e50-e830-46f9-b20b-5aeb0c0040ad',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        BlocConsumer<JoinGroupPageCubit, JoinGroupPageState>(
-                          bloc: context.read<JoinGroupPageCubit>(),
-                          listener: (context, state) {
-                            if (state.status == JoinGroupPageStatus.error) {
-                              ScaffoldMessenger.of(context)
-                                ..clearSnackBars()
-                                ..showSnackBar(SnackBar(
-                                    backgroundColor:
-                                        Theme.of(context).colorScheme.error,
-                                    content: Text(state.errorMessage)));
-                            }
-                            if (state.status == JoinGroupPageStatus.success) {
-                              Navigator.of(context).pop();
-                            }
-                          },
-                          listenWhen: (previous, current) =>
-                              previous.status != current.status,
-                          buildWhen: (previous, current) =>
-                              previous.status != current.status,
-                          builder: (context, state) {
-                            return FilledButton(
-                                onPressed:
-                                    state.status == JoinGroupPageStatus.loading
-                                        ? null
-                                        : _submitForm,
-                                child: Text(
-                                    state.status == JoinGroupPageStatus.loading
-                                        ? 'Joining...'
-                                        : 'Join challenge'));
-                          },
-                        ),
-                      ],
-                    )
-                  ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: AppSpacings.horizontalPadding.toDouble()),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                Text(
+                  'Join Group',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
-              ))),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Enter a group code to join',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  validator: (value) => FormValidators.validateGroupCode(value),
+                  onChanged: (value) {
+                    //widget.joinGroupPageCubit.updateGroupCode(value);
+                    context.read<JoinGroupPageCubit>().updateGroupCode(value);
+                  },
+                  decoration: InputDecoration(
+                      label: Text('Group code'), border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Looks like',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  '• afb78e50-e830-46f9-b20b-5aeb0c0040ad',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    BlocConsumer<JoinGroupPageCubit, JoinGroupPageState>(
+                      bloc: context.read<JoinGroupPageCubit>(),
+                      listener: (context, state) {
+                        if (state.status == JoinGroupPageStatus.error) {
+                          ScaffoldMessenger.of(context)
+                            ..clearSnackBars()
+                            ..showSnackBar(SnackBar(
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.error,
+                                content: Text(state.errorMessage)));
+                        }
+                        if (state.status == JoinGroupPageStatus.success) {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      listenWhen: (previous, current) =>
+                          previous.status != current.status,
+                      buildWhen: (previous, current) =>
+                          previous.status != current.status,
+                      builder: (context, state) {
+                        return FilledButton(
+                          onPressed: state.status == JoinGroupPageStatus.loading
+                              ? null
+                              : _submitForm,
+                          child: Text(
+                              state.status == JoinGroupPageStatus.loading
+                                  ? 'Joining...'
+                                  : 'Join challenge'),
+                        );
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
-    void _submitForm() {
+
+  void _submitForm() {
     if (!_formKey.currentState!.validate()) return;
     context.read<JoinGroupPageCubit>().submitForm();
   }
