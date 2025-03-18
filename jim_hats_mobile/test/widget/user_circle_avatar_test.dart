49932cd2745d642822767c266264317ee78ecaf9
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-18 14:07:43 -0300

    add widgets tests

---
+++ jim_hats_mobile/test/widget/user_circle_avatar_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jim_hats_mobile/presentation/widgets/user_circle_avatar/user_circle_avatar.dart';
+
+void main() {
+  testWidgets(
+    'Should display initials (Uppercased) if no avatar url is provided',
+    (widgetTester) async {
+      await widgetTester.pumpWidget(
+        MaterialApp(
+          home: UserCircleAvatar(
+            avatarUrl: null,
+            username: 'Mateus',
+          ),
+        ),
+      );
+
+      final initialsFinder = find.text('MA');
+      expect(initialsFinder,findsOne);
+    },
+  );
+}
