eae44f10b25e67cb70db7831a921ea68e2ab989e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-19 20:37:15 -0300

    add splash page test

---
+++ jim_hats_mobile/test/widget/splash_page_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jim_hats_mobile/presentation/views/splash/splah_page.dart';
+
+void main() {
+  testWidgets('Splash page should display circular progress indicator',(widgetTester) async {
+      await widgetTester.pumpWidget(MaterialApp(home: SplahPage()));
+      expect(find.byType(CircularProgressIndicator),findsExactly(1));
+  },);
+}
