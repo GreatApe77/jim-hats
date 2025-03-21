9006ef7d316d09cc72b85c0a200ab528534db84d
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-21 13:31:51 -0300

    add tests for sign in page bloc

--- jim_hats_mobile/lib/presentation/blocs/sign_in_page/sign_in_page_bloc.dart
+++ jim_hats_mobile/lib/presentation/blocs/sign_in_page/sign_in_page_bloc.dart
@@ -37,7 +37,8 @@ class SignInPageBloc extends Bloc<SignInPageEvent, SignInPageState> {
             message: e.getMessage(),
           ));
         } catch (e) {
-          emit(state.copywith(status: SignInPageStatus.failure));
+          
+          emit(state.copywith(status: SignInPageStatus.failure,message: 'Unknown error while signing in'));
           //emit(SignInPageState.empty());
         }
       },
--- jim_hats_mobile/test/unit/blocs/sign_in_page/sign_in_page_bloc_test.dart
+++ jim_hats_mobile/test/unit/blocs/sign_in_page/sign_in_page_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jim_hats_mobile/core/exceptions/time_out_exception.dart';
+import 'package:jim_hats_mobile/data/auth/dtos/login_dto.dart';
 import 'package:jim_hats_mobile/data/auth/repositories/auth_repository.dart';
 import 'package:jim_hats_mobile/presentation/blocs/sign_in_page/sign_in_page_bloc.dart';
 import 'package:mockito/annotations.dart';
@@ -75,12 +76,12 @@ void main() {
       );
       blocTest<SignInPageBloc, SignInPageState>(
         'Should submit the form but throws a expected Application error',
-        build: () {
-          when(mockAuthRepository.login(null)).thenAnswer(
-            (_) async => throw sampleTimeoutException,
+        setUp: () {
+          when(mockAuthRepository.login(any)).thenThrow(
+            sampleTimeoutException
           );
-          return SignInPageBloc(authRepository: mockAuthRepository);
         },
+        build: () => sut,
         act: (bloc) => bloc.add(SignInFormSubmitted()),
         expect: () => [
           isA<SignInPageState>().having(
@@ -98,6 +99,34 @@ void main() {
                 (state) => state.message,
                 'error message',
                 sampleTimeoutException.getMessage(),
+              ),
+        ],
+      );
+       blocTest<SignInPageBloc, SignInPageState>(
+        'Should submit the form but throws an unknown error',
+        setUp: () {
+          when(mockAuthRepository.login(any)).thenThrow(
+            Exception()
+          );
+        },
+        build: () => sut,
+        act: (bloc) => bloc.add(SignInFormSubmitted()),
+        expect: () => [
+          isA<SignInPageState>().having(
+            (state) => state.status,
+            'status',
+            SignInPageStatus.loading,
+          ),
+          isA<SignInPageState>()
+              .having(
+                (state) => state.status,
+                'status',
+                SignInPageStatus.failure,
+              )
+              .having(
+                (state) => state.message,
+                'error message',
+                'Unknown error while signing in'
               ),
         ],
       );
