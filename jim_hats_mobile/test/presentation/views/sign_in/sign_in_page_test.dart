444c77696a7d8c061987ae09a3698a5f757dd65d
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-24 16:46:07 -0300

    sign in page tests wip

--- jim_hats_mobile/test/presentation/views/sign_in/sign_in_page_test.dart
+++ jim_hats_mobile/test/presentation/views/sign_in/sign_in_page_test.dart
@@ -6,14 +6,15 @@ import 'package:jim_hats_mobile/presentation/blocs/sign_in_page/sign_in_page_blo
 import 'package:jim_hats_mobile/presentation/views/sign_in/sign_in_page.dart';
 import 'package:mocktail/mocktail.dart';
 
-
 class _MockSignInPageBloc extends MockBloc<SignInPageEvent, SignInPageState>
     implements SignInPageBloc {}
 
 //@GenerateNiceMocks([MockSpec<SignInPageBloc>()])
 void main() {
-  
   late _MockSignInPageBloc mockSignInPageBloc;
+  final signInBtnKey = Key('SignInView.sign_in_button');
+  final usernameTextFieldKey = Key('SignInView.username_field');
+  final passwordTextFieldKey = Key('SignInView.password_field');
   setUp(
     () {
       mockSignInPageBloc = _MockSignInPageBloc();
@@ -24,32 +25,103 @@ void main() {
   );
   tearDown(
     () {
-      
       locator.unregister<SignInPageBloc>();
     },
   );
-  // tearDownAll(
-  //   () async {
-  //     locator.unregister<SignInPageBloc>();
-  //   },
-  // );
-  testWidgets('Should update bloc state when typing on username form field',
-      (tester) async {
+
+  testWidgets('Should display initial data', (tester) async {
     when(() => mockSignInPageBloc.state).thenReturn(
       SignInPageState(
-          username: 'username',
+          username: '',
           status: SignInPageStatus.writingForm,
           password: '',
           message: ''),
     );
-    
+
     await tester.pumpWidget(MaterialApp(
       home: SignInPage(),
     ));
-    final usernameTextField = find.byKey(Key('SignInView.username_field'));
+    final usernameTextField = find.byKey(usernameTextFieldKey);
+    final passwordTextField = find.byKey(passwordTextFieldKey);
+    final signInBtn = find.byKey(signInBtnKey);
+    expect(usernameTextField, findsOne);
+    expect(passwordTextField, findsOne);
+    expect(signInBtn, findsOne);
+  });
+  testWidgets('Should update bloc state when typing on username form field',
+      (tester) async {
+    when(() => mockSignInPageBloc.state).thenReturn(
+      SignInPageState(
+          username: '',
+          status: SignInPageStatus.writingForm,
+          password: '',
+          message: ''),
+    );
+
+    await tester.pumpWidget(MaterialApp(
+      home: SignInPage(),
+    ));
+    final usernameTextField = find.byKey(usernameTextFieldKey);
 
     await tester.enterText(usernameTextField, 'Mateus');
     //await tester.pumpAndSettle();
-    verify(() => mockSignInPageBloc.add(SignInUsernameChanged(username: 'Mateus')),).called(1);
+    verify(
+      () => mockSignInPageBloc.add(SignInUsernameChanged(username: 'Mateus')),
+    ).called(1);
   });
+  testWidgets('Should update bloc state when typing on password form field',
+      (tester) async {
+    when(
+      () => mockSignInPageBloc.state,
+    ).thenReturn(
+      SignInPageState(
+          username: '',
+          status: SignInPageStatus.writingForm,
+          password: '',
+          message: ''),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: SignInPage(),
+      ),
+    );
+    final passwordTextField = find.byKey(passwordTextFieldKey);
+
+    await tester.enterText(passwordTextField, 'secret');
+    await tester.pumpAndSettle();
+    final typedText = find.textContaining('secret');
+    expect(typedText, findsOne);
+    //await tester.pumpAndSettle();
+    verify(
+      () => mockSignInPageBloc.add(SignInPasswordChanged(password: 'secret')),
+    ).called(1);
+  });
+  testWidgets(
+    'Should display error messages when form is submitted with empty values',
+    (tester) async {
+      when(
+        () => mockSignInPageBloc.state,
+      ).thenReturn(
+        SignInPageState(
+            username: '',
+            status: SignInPageStatus.writingForm,
+            password: '',
+            message: ''),
+      );
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: SignInPage(),
+        ),
+      );
+      final signInBtn = find.byKey(signInBtnKey);
+      await tester.tap(signInBtn);
+      await tester.pumpAndSettle();
+      final requiredUsernameErrorMessage = 'Username is required';
+      final requiredPasswordErrorMessage = 'Password is required';
+      expect(find.textContaining(requiredUsernameErrorMessage), findsOne);
+      expect(find.textContaining(requiredPasswordErrorMessage), findsOne);
+    },
+  );
 }
