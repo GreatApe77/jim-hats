945c15c3072fc90d9f88f8b693d3df0495d52b3a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-03-27 13:24:46 -0300

    add tests for ranking page

--- jim_hats_mobile/lib/presentation/views/ranking/ranking_page.dart
+++ jim_hats_mobile/lib/presentation/views/ranking/ranking_page.dart
@@ -41,81 +41,86 @@ class RankingView extends StatelessWidget {
           }
           if (state is RankingPagedDataLoadSuccess) {
             return SafeArea(
-                child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: AppSpacings.horizontalPadding),
-                    child: ListView(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 10),
-                          child: Text(
-                            state.challenge.name,
-                            style: Theme.of(context).textTheme.headlineSmall,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: AppSpacings.horizontalPadding),
+                child: ListView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        state.challenge.name,
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ),
+                    LinearProgressIndicator(
+                      value: _getRemainingDaysPercentage(
+                          startDate: state.challenge.startAt,
+                          endDate: state.challenge.endAt),
+                      minHeight: 20,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Starts ${DateHelper.formatDateShort(state.challenge.startAt)}',
+                            style: Theme.of(context).textTheme.bodySmall,
                           ),
-                        ),
-                        LinearProgressIndicator(
-                          value: _getRemainingDaysPercentage(
-                              startDate: state.challenge.startAt,
-                              endDate: state.challenge.endAt),
-                          minHeight: 20,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Starts ${DateHelper.formatDateShort(state.challenge.startAt)}',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              Text(
-                                'Finishes ${DateHelper.formatDateShort(state.challenge.endAt)}',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                            ],
+                          Text(
+                            'Finishes ${DateHelper.formatDateShort(state.challenge.endAt)}',
+                            style: Theme.of(context).textTheme.bodySmall,
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Text(
-                            'Rankings',
-                            style: Theme.of(context).textTheme.titleMedium,
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Rankings',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                    ListView.builder(
+                      itemCount: state.rankings.length,
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(state.rankings[index].username),
+                          subtitle: Text(
+                              '${state.rankings[index].logCount} days active'),
+                          leading: UserCircleAvatar(
+                            username: state.rankings[index].username,
+                            avatarUrl: state.rankings[index].profilePicture,
                           ),
-                        ),
-                        ListView.builder(
-                          itemCount: state.rankings.length,
-                          shrinkWrap: true,
-                          physics: ClampingScrollPhysics(),
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(state.rankings[index].username),
-                              subtitle: Text(
-                                  '${state.rankings[index].logCount} days active'),
-                              leading: UserCircleAvatar(
-                                username: state.rankings[index].username,
-                                avatarUrl: state.rankings[index].profilePicture,
-                              ),
-                            );
-                          },
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 10),
-                          child: Text(
-                            'Group stats',
-                            style: Theme.of(context).textTheme.titleMedium,
-                          ),
-                        ),
-                        ListTile(
-                          title: Text(
-                              '${context.read<RankingPageCubit>().countTotalOfLogs(state.rankings)}'),
-                          subtitle: Text('Total check-ins'),
-                          leading: Icon(Icons.monitor_heart),
-                        )
-                      ],
-                    )));
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        'Group stats',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                          key: Key('RankingView.total_count_text'),
+                          '${context.read<RankingPageCubit>().countTotalOfLogs(state.rankings)}'),
+                      subtitle: Text('Total check-ins'),
+                      leading: Icon(Icons.monitor_heart),
+                    )
+                  ],
+                ),
+              ),
+            );
           }
-          return SizedBox.shrink(key: Key('RankingView.shrinked_sized_box'),);
+          return SizedBox.shrink(
+            key: Key('RankingView.shrinked_sized_box'),
+          );
         },
       ),
     );
--- jim_hats_mobile/test/presentation/views/ranking/ranking_page_test.dart
+++ jim_hats_mobile/test/presentation/views/ranking/ranking_page_test.dart
@@ -1,10 +1,13 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:jim_hats_mobile/data/gym_challenges/models/gym_challenge.dart';
+import 'package:jim_hats_mobile/data/gym_challenges/models/ranking.dart';
 import 'package:jim_hats_mobile/locator.dart';
 import 'package:jim_hats_mobile/presentation/cubits/ranking_page/ranking_page_cubit.dart';
 import 'package:jim_hats_mobile/presentation/views/ranking/ranking_page.dart';
 import 'package:jim_hats_mobile/presentation/views/ranking/ranking_page_arguments.dart';
+import 'package:mocktail/mocktail.dart';
 
 class MockRankingPageCubit extends MockCubit<RankingPageState>
     implements RankingPageCubit {}
@@ -12,11 +15,41 @@ class MockRankingPageCubit extends MockCubit<RankingPageState>
 void main() {
   late RankingPageCubit mockRankingPageCubit;
   final sampleRankingPageArguments = RankingPageArguments(challengeId: 1);
+  final sampleChallenge = GymChallenge(
+    id: 1,
+    name: 'Sample Challenge',
+    description: 'this is a simple challenge',
+    createdAt: DateTime(2025),
+    startAt: DateTime(2025),
+    endAt: DateTime(2026),
+    creatorId: 1,
+  );
+  final sampleRankings = [
+    Ranking(
+      id: 1,
+      username: 'Mateus',
+      logCount: 99,
+    ),
+    Ranking(
+      id: 2,
+      username: 'John',
+      logCount: 150,
+    ),
+    Ranking(
+      id: 5,
+      username: 'Arthur',
+      logCount: 131,
+    )
+  ];
   setUp(
     () {
       mockRankingPageCubit = MockRankingPageCubit();
       locator.registerFactory<RankingPageCubit>(
         () => mockRankingPageCubit,
+      );
+      when(() => mockRankingPageCubit
+          .loadData(sampleRankingPageArguments.challengeId)).thenAnswer(
+        (_) async {},
       );
     },
   );
@@ -25,19 +58,60 @@ void main() {
       locator.unregister<RankingPageCubit>();
     },
   );
-  // testWidgets('Should display ranking data', (tester) async {
-  //   whenListen(
-  //     mockRankingPageCubit,
-  //     Stream<RankingPageState>.fromIterable([]),
-  //     initialState: RankingPageInitial(),
-  //   );
+  testWidgets('Should display ranking data', (tester) async {
+    whenListen(
+      mockRankingPageCubit,
+      Stream<RankingPageState>.fromIterable([]),
+      initialState: RankingPageInitial(),
+    );
 
-  //   await tester.pumpWidget(
-  //     MaterialApp(
-  //       home: RankingPage(rankingPageArguments: sampleRankingPageArguments),
-  //     ),
-  //   );
-  //   await tester.pump();
-  //   expect(find.byKey(Key('RankingView.shrinked_sized_box')), findsOneWidget);
-  // });
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RankingPage(rankingPageArguments: sampleRankingPageArguments),
+      ),
+    );
+    await tester.pump();
+    expect(find.byKey(Key('RankingView.shrinked_sized_box')), findsOneWidget);
+  });
+  testWidgets('Should display circular progress indicator when loading',
+      (tester) async {
+    whenListen(
+      mockRankingPageCubit,
+      Stream<RankingPageState>.fromIterable([RankingPageDataLoadInProgress()]),
+      initialState: RankingPageInitial(),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RankingPage(rankingPageArguments: sampleRankingPageArguments),
+      ),
+    );
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    //expect(find.byKey(Key('RankingView.shrinked_sized_box')), findsOneWidget);
+  });
+  testWidgets('Should display page data', (tester) async {
+    whenListen(
+      mockRankingPageCubit,
+      Stream<RankingPageState>.fromIterable([
+        RankingPagedDataLoadSuccess(
+          challenge: sampleChallenge,
+          rankings: sampleRankings,
+        )
+      ]),
+      initialState: RankingPageInitial(),
+    );
+    when(
+      () => mockRankingPageCubit.countTotalOfLogs(sampleRankings),
+    ).thenReturn(150);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RankingPage(rankingPageArguments: sampleRankingPageArguments),
+      ),
+    );
+    await tester.pump();
+    expect(find.byKey(Key('RankingView.total_count_text')),findsOneWidget);
+    expect(find.text('150'), findsOne);
+    //expect(find.byKey(Key('RankingView.shrinked_sized_box')), findsOneWidget);
+  });
 }
