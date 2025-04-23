a61178d80185554456dfd271a2b55f11a334a4c5
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-04-23 16:34:11 -0300

    request notification permissions

--- jim_hats_mobile/lib/main.dart
+++ jim_hats_mobile/lib/main.dart
@@ -1,19 +1,44 @@
 // coverage:ignore-file
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jim_hats_mobile/app.dart';
 import 'package:jim_hats_mobile/core/network/httpfix/my_http_overrides.dart';
 import 'package:jim_hats_mobile/locator.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
+  await setupDependencies();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  HttpOverrides.global = MyHttpOverrides();
-  await setupDependencies();
+
+  final messaging = FirebaseMessaging.instance;
+
+  final settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (kDebugMode) {
+    print('Permission granted: ${settings.authorizationStatus}');
+  }
+  // It requests a registration token for sending messages to users from your App server or other trusted server environment.
+  String? token = await messaging.getToken();
+
+  if (kDebugMode) {
+    print('Registration Token=$token');
+  }
   runApp(MultiBlocProvider(providers: blocProviders, child: const App()));
 }
