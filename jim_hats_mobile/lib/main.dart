ec435a92050603ee088e0c3bf0434da0162b9196
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-04-23 13:31:17 -0300

    add firebase config

--- jim_hats_mobile/lib/main.dart
+++ jim_hats_mobile/lib/main.dart
@@ -1,6 +1,7 @@
 // coverage:ignore-file
 import 'dart:io';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jim_hats_mobile/app.dart';
@@ -9,6 +10,9 @@ import 'package:jim_hats_mobile/locator.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   HttpOverrides.global = MyHttpOverrides();
   await setupDependencies();
   runApp(MultiBlocProvider(providers: blocProviders, child: const App()));
