413ccefb697c30797062607209f54ebdc3d1d439
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2026-01-24 19:40:49 -0300

    feat: Enforce unique constraint on username in users table

--- jim-hats-web-api/src/main/resources/db/migration/V1__initial_table_creations.sql
+++ jim-hats-web-api/src/main/resources/db/migration/V1__initial_table_creations.sql
@@ -18,7 +18,7 @@ CREATE TABLE "users" (
     "id" BIGSERIAL PRIMARY KEY,
     "first_name" VARCHAR(255) NOT NULL,
     "last_name" VARCHAR(255) NOT NULL,
-    "username" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(255) UNIQUE NOT NULL,
     "email" VARCHAR(255) UNIQUE NOT NULL,
     "profile_picture_url" VARCHAR(255),
     "password" VARCHAR(255) NOT NULL,
