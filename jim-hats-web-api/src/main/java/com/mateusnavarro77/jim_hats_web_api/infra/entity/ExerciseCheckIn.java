513513dbe5a2a820e7ce58e23dba61974d4d5ab6
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2026-01-24 19:36:53 -0300

    feat: Implement initial database schema with PostgreSQL and create entity classes for user management and gym challenges

---
+++ jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/infra/entity/ExerciseCheckIn.java
@@ -0,0 +1,43 @@
+package com.mateusnavarro77.jim_hats_web_api.infra.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "exercise_check_ins")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ExerciseCheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "exercise_img_url", nullable = false)
+    private String exerciseImgUrl;
+}
---
+++ jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/infra/entity/User.java
@@ -0,0 +1,49 @@
+package com.mateusnavarro77.jim_hats_web_api.infra.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "profile_picture_url")
+    private String profilePictureUrl;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDate updatedAt;
+}
