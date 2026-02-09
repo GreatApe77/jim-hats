8d406214ab659f66b726c49f77b742eed2f3b9f9
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2026-02-09 10:58:15 -0300

    feat: add createSystemRole endpoint and corresponding service method for role creation

--- jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/auth/controller/SystemRoleController.java
+++ jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/auth/controller/SystemRoleController.java
@@ -1,16 +1,23 @@
 package com.mateusnavarro77.jim_hats_web_api.auth.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mateusnavarro77.jim_hats_web_api.auth.dto.CreateSystemRoleDto;
 import com.mateusnavarro77.jim_hats_web_api.auth.entity.SystemRole;
 import com.mateusnavarro77.jim_hats_web_api.auth.service.SystemRoleService;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 
 @RestController
@@ -30,7 +37,19 @@ public class SystemRoleController {
 
     @GetMapping("/{systemRoleId}")
     @PreAuthorize("@authorizationService.check(authentication, 'ROLES:READ')")
-    public SystemRole getAllSystemRoles(@PathVariable @Min(1) Long systemRoleId) {
+    public SystemRole getSystemRoleById(@PathVariable @Min(1) Long systemRoleId) {
         return this.systemRoleService.getSystemRoleById(systemRoleId);
+    }
+
+    @PostMapping
+    @PreAuthorize("@authorizationService.check(authentication, 'ROLES:CREATE')")
+    public ResponseEntity<?> createSystemRole(@RequestBody @Valid CreateSystemRoleDto createSystemRoleDto) {
+        var systemRole = this.systemRoleService.createSystemRole(createSystemRoleDto.systemRoleName());
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(systemRole.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/auth/service/SystemRoleService.java
+++ jim-hats-web-api/src/main/java/com/mateusnavarro77/jim_hats_web_api/auth/service/SystemRoleService.java
@@ -9,6 +9,8 @@ import org.springframework.web.server.ResponseStatusException;
 import com.mateusnavarro77.jim_hats_web_api.auth.entity.SystemRole;
 import com.mateusnavarro77.jim_hats_web_api.auth.repository.SystemRoleRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class SystemRoleService {
     private final SystemRoleRepository systemRoleRepository;
@@ -24,5 +26,16 @@ public class SystemRoleService {
     public SystemRole getSystemRoleById(Long systemRoleId) {
         return this.systemRoleRepository.findById(systemRoleId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "System role not found"));
+    }
+
+    @Transactional
+    public SystemRole createSystemRole(
+            String systemRoleName) {
+        if (this.systemRoleRepository.findByName(systemRoleName).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "System role with the same name already exists");
+        }
+        var systemRole = new SystemRole();
+        systemRole.setName(systemRoleName);
+        return this.systemRoleRepository.save(systemRole);
     }
 }
